088b1a03a4120b53ea50bf68195386d79ba58079
SAI TEJA <kommisaiteja677@gmail.com>
2025-02-09 22:14:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const userInput = document.getElementById("user-input");
 
 async function fetchGeminiResponse(message) {
     try {
-        const response = await fetch('http://localhost:3030/response', {
+        const response = await fetch('https://semester-project-2ln5.onrender.com', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
